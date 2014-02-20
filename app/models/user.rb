db4125b7f6b35e4598081dc6962fc1eb1a542e06
Quest Henkart <qhenkart@gmail.com>
2014-02-20 00:02:32 -0800

    finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+	has_secure_password
 	before_save {email.downcase!}
 	validates :name, presence: true, length: {maximum: 50}
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
@@ -6,7 +7,7 @@ class User < ActiveRecord::Base
 									  format: {with: VALID_EMAIL_REGEX }, 
 									  uniqueness: { case_sensitive: false}
 	
-	has_secure_password
+	
 	validates :password, length: {minimum: 6}
 
 	
