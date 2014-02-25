49f8bbafdc697093ad61c54c4897afd12eefa5f9
Quest Henkart <qhenkart@gmail.com>
2014-02-24 22:42:56 -0800

    almost finished with sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  name            :string(255)
+#  email           :string(255)
+#  created_at      :datetime
+#  updated_at      :datetime
+#  password_digest :string(255)
+#
+
 require 'spec_helper'
 
 describe User do
@@ -11,6 +23,7 @@ describe User do
   it { should respond_to(:password_digest)}
   it { should respond_to(:password)}
   it { should respond_to(:password_confirmation)}
+  it { should respond_to(:remember_token)}
   it { should respond_to(:authenticate)}
   it {should be_valid}
 
@@ -105,6 +118,10 @@ describe User do
 
   end
 
+  describe "remember token" do
+    before { @user.save}
+    its(:remember_token) {should_not be_blank}
+  end
 
 end
 
