ee248ff936454d927424196f47a4479bacd58706
Quest Henkart <qhenkart@gmail.com>
2014-02-26 16:38:42 -0800

    add user microposts and user feed chapter 10 finished

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+	has_many :microposts, dependent: :destroy
 	has_secure_password
 	before_save {email.downcase!}
 	before_create :create_remember_token
@@ -19,6 +20,11 @@ end
 
 def User.encrypt(token)
 	Digest::SHA1.hexdigest(token.to_s)
+end
+
+def feed
+	#this is preliminary. See following users for full implenetation
+	Micropost.where("user_id = ?", id)
 end
 
 private
